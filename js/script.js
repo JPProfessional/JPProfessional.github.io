f374bc5590ffc0c4983d2b205a09b6f6017be478
JPProfessional <perrin.john@gmail.com>
2022-01-23 21:43:48 -0700

    Added CSS for expCompanyContainer and children, more work needed

--- js/script.js
+++ js/script.js
@@ -98,9 +98,9 @@ const resumeData = {
         {"name": "Calmena", "website": "", "position": "Business Development Manager", "Summary": "Joined a hungry oil field services start up and grew our share of the Microseismic Fracture Monitoring and Horizontal Completion Technology market in Canada.", "location": "Canada, Calgary", "startDate": "2010-12-13", "endDate": "2012-12-18", "highlights": []},
         {"name": "Enerplus Corporation", "website": "www.enerplus.com", "position": "Staff Petrophysical Engineer", "Summary": "Recruited by the Chief Geoscientist to expand the rigorous use of Petrophysics across Enerplus. Established a foundation of Petrophysical knowledge throughout the organization by performing advanced analysis and delivering formal and informal training to geoscientists, engineers and managers.", "location": "Canada, Calgary", "startDate": "2012-12-18", "endDate": "2015-06-02", "highlights": []},
         {"name": "NXT Energy Solutions", "website": "www.nxtenergy.com", "position": "Business Devlopment Manager", "Summary": "", "location": "Canada, Calgary", "startDate": "2016-12-01", "endDate": "2017-02-22", "highlights": []},
-        {"name": "Weatherford now Stratum Reservoir", "website": "www.stratumreservoir.com", "position": "Product Line Manager - Reservoir Services (Laboratories and Sub-Surface Consulting)", "Summary": "Recruited to lead a technically strong but unprofitable Geoscience, Fluid PVT (Pressure Volume Temperature) and SCAL (Special Core Analysis) laboratory. Implemented a cultural change and renewed focus on oilfield service fundamentals.", "location": "Canada, Calgary", "startDate": "2017-04-01", "endDate": "2018-06-21", "highlights": []},
+        {"name": "Weatherford", "website": "www.stratumreservoir.com", "position": "Product Line Manager - Reservoir Services (Laboratories and Sub-Surface Consulting)", "Summary": "Recruited to lead a technically strong but unprofitable Geoscience, Fluid PVT (Pressure Volume Temperature) and SCAL (Special Core Analysis) laboratory. Implemented a cultural change and renewed focus on oilfield service fundamentals.", "location": "Canada, Calgary", "startDate": "2017-04-01", "endDate": "2018-06-21", "highlights": []},
         {"name": "DIKUW Incorporated", "website": "www.dikuw.ca", "position": "Founder", "Summary": "DIKUW specializes in enabling YOUR TEAM to overcome barriers to success. DIKUW collaborates with a focus on fundamental principles to assess, suggest and implement the optimal solution — all to provide exceptional advice that will transform your business and provide value for years to come.", "location": "Canada, Calgary", "startDate": "2019-03-01", "endDate": "2022-01-19", "highlights": [ ]},
-        {"name": "DIKUW Incorporated", "website": "www.ceda.com", "position": "Consulting Engagement to CEDA, Technical Center of Excellence", "Summary": "Coached CEDA’s newly founded “Technical Center of Exellence” using DIKUW’s proprietary  “Comprehensive Framework for Field Operations Project Management” and general knowledge of how to deploy technical resources for successful commercial outcomes.’", "location": "Canada, Calgary", "startDate": "2019-03-01", "endDate": "2019-09-15", "highlights": [ ]},
+        {"name": "DIKUW Incorporated", "website": "www.ceda.com", "position": "Consulting Engagement to CEDA International Corp.", "Summary": "Coached CEDA’s newly founded “Technical Center of Exellence” using DIKUW’s proprietary  “Comprehensive Framework for Field Operations Project Management” and general knowledge of how to deploy technical resources for successful commercial outcomes.’", "location": "Canada, Calgary", "startDate": "2019-03-01", "endDate": "2019-09-15", "highlights": [ ]},
         {"name": "Emerson Automation Solutions", "website": "www.zedisolutions.com", "position": "Team Lead, Customer Solutions Engineering, Zedi Cloud SCADA (IoT SaaS Platform)", "Summary": "", "location": "Canada, Calgary", "startDate": "2019-05-01", "endDate": "2019-08-01", "highlights": [ ]},
         {"name": "Emerson Automation Solutions", "website": "www.zedisolutions.com", "position": "Manager, Automation Engineering, Zedi Cloud SCADA (IoT SaaS Platform)", "Summary": "", "location": "Canada, Calgary", "startDate": "2019-08-01", "endDate": "2020-02-01", "highlights": [ ]},
         {"name": "Emerson Automation Solutions", "website": "www.zedisolutions.com", "position": "Director, Automation Engineering, Zedi Cloud SCADA (IoT SaaS Platform)", "Summary": "Achieved an across the board KPI improvement of Automation Engineering Sales, Implementation and and Support", "location": "Canada, Calgary", "startDate": "2020-02-01", "endDate": "2020-10-01", "highlights": []},
@@ -130,10 +130,12 @@ var logoImages = [
 ]
 
 
-//Parse resume object with stringify to overcome common error LMGTFY 
+//Parse resume object with stringify to overcome common error LMGTFY and sort to reverseChron
 const work = JSON.stringify(resumeData.work);
-const workParsed = JSON.parse(work);
+var workParsed = JSON.parse(work).sort((a,b) => new Date(b.endDate) - new Date(a.endDate));
+workParsed.forEach( (item) => calculateDuration(item)); //Add durationPhrase
 //Add numeric date field to our objects
+workParsed.forEach(job => calculateDuration(job));
 workParsed.forEach(job => job.startDateNumber = Date.parse(job.startDate));
 workParsed.forEach(job => job.endDateNumber = Date.parse(job.endDate));
 delete workParsed.website
@@ -141,15 +143,12 @@ delete workParsed.website
 // Create an object of unique companies worked and populate with summary data
 const workSummaryList = [...new Set(workParsed.map( item => item.name))];
 var workSummary = [];
-// const total = Object.values(add).reduce((t, {value}) => t + value, 0)
 var startDateSort
 workSummaryList.forEach( (company, index) => {
   const regex = RegExp(company);
   const nameSet = workParsed.filter(({name}) => name.match(regex));
-  // console.log({nameSet});
   //Create company Meta Data if company has 1 entry
   if ( !nameSet[1]) {
-    // console.log({regex});
     var record = {
       "name": company,
       "startDate": workParsed.find(({ name }) => name.match(RegExp(regex))).startDate,
@@ -159,7 +158,7 @@ workSummaryList.forEach( (company, index) => {
   } else {
     const nameStartDate = nameSet.map(a => a.startDate).reduce(function(min, cur) {
       return cur < min ? cur : min;
-    },"210000");
+    },"21000");
     const nameEndDate = nameSet.map(a => a.endDate).reduce(function(max, cur) {
       return cur > max ? cur : max;
     },"0");
@@ -169,15 +168,15 @@ workSummaryList.forEach( (company, index) => {
       "endDate":nameEndDate
     }
   }
-  if (index == 0) {
-    workSummary[index] = record;
-  } else {
-    workSummary[index] = record;
-  };
-  
+  calculateDuration(record);//Add durationPhrase
+  workSummary[index] = record;//Add record to summary object
+
 });
 
-
+var workSummarySorted = workSummary.sort();
+workSummary = workSummarySorted;
+delete workSummary.startDate;
+delete workSummary.endDate;
 var expTitle = document.createElement('h5');
 Object.assign( expTitle, {
   className: "sectionTitle",
@@ -188,60 +187,133 @@ expContainer.className = "expContainer"
 expContainer.appendChild(expTitle);
 
 
-workSummary.forEach( (summary, index) => {
+//Create Exerience Summary DOM
+workSummary.forEach( (summary) => {
   
   const companyNameCollapsed = summary.name.split(" ").join("");
-  
+
   //Create company container with logo
-  eval('var ' + companyNameCollapsed + "Container = document.createElement('div')");
-  Object.assign( eval(companyNameCollapsed + "Container"), {
+  eval(`var ${companyNameCollapsed}Container = document.createElement('div')`);
+  Object.assign( eval(`${companyNameCollapsed}Container`), {
     className: 'expCompanyContainer'
   });
-  eval('var ' + companyNameCollapsed + "LogoContainer = document.createElement('div')");
-  Object.assign( eval(companyNameCollapsed + "LogoContainer"), {
+  eval(`var ${companyNameCollapsed}LogoContainer = document.createElement('div')`);
+  Object.assign( eval(`${companyNameCollapsed}LogoContainer`), {
     className: 'expCompanyLogoContainer'
   });
-  eval('var ' + companyNameCollapsed + "LogoHref = document.createElement('a')");
-  Object.assign( eval(companyNameCollapsed + "LogoHref"), {
+  eval(`var ${companyNameCollapsed}TextContainer = document.createElement('div')`);
+  Object.assign( eval(`${companyNameCollapsed}TextContainer`), {
+    className: 'expCompanyTextContainer'
+  });
+  eval(`var ${companyNameCollapsed}LogoHref = document.createElement('a')`);
+  Object.assign( eval(`${companyNameCollapsed}LogoHref`), {
     className: 'expCompanyHref',
     href: logoImages.find(({ name }) => name.match(RegExp(companyNameCollapsed))).url
   });
 
-  eval('var ' + companyNameCollapsed + "Logo = document.createElement('img')");
-  Object.assign( eval(companyNameCollapsed + "Logo"), {
+  eval(`var ${companyNameCollapsed}Logo = document.createElement('img')`);
+  Object.assign( eval(`${companyNameCollapsed}Logo`), {
     className: 'companyLogoImage',
     alt: summary.name,
-    // "a href": logoImages.find(({ name }) => name.match(RegExp(companyNameCollapsed))).file,
     src: logoImages.find(({ name }) => name.match(RegExp(companyNameCollapsed))).file
   });
-  eval(companyNameCollapsed + "LogoHref").appendChild(eval(eval(companyNameCollapsed + "Logo")));
-  eval(companyNameCollapsed + "LogoContainer").appendChild(eval(eval(companyNameCollapsed + "LogoHref")));
-  eval(companyNameCollapsed + "Container").appendChild(eval(companyNameCollapsed + "LogoContainer"));
-  expContainer.appendChild(eval(companyNameCollapsed + "Container"));
+  eval(`${companyNameCollapsed}LogoHref`).appendChild(eval(eval(`${companyNameCollapsed}Logo`)));
+  eval(`${companyNameCollapsed}LogoContainer`).appendChild(eval(eval(`${companyNameCollapsed}LogoHref`)));
+  eval(`${companyNameCollapsed}Container`).appendChild(eval(`${companyNameCollapsed}LogoContainer`));
+  eval(`${companyNameCollapsed}Container`).appendChild(eval(`${companyNameCollapsed}TextContainer`));
+  expContainer.appendChild(eval(`${companyNameCollapsed}Container`));
 
   
-  //Create company summary row
-  eval('var ' + companyNameCollapsed + "SummaryRow = document.createElement('div')");
-  Object.assign( eval(companyNameCollapsed + "SummaryRow"), {
+  //Create expSummaryRow
+  eval(`var ${companyNameCollapsed}SummaryRow = document.createElement('div')`);
+  Object.assign( eval(`${companyNameCollapsed}SummaryRow`), {
         className: 'expSummaryRow',
         // textContent: summary.name
   });
   
-  //Create entries for company summary row
+  //Create expDetail
   Object.entries(summary).forEach(detail => {
-    const detailType = 'expDetail' + (detail[0].charAt(0).toUpperCase() + detail[0].slice(1));
-    eval('var ' + detailType + " = document.createElement('p')");
+    console.log({detail});
+    const detailType = `expDetail${detail[0].charAt(0).toUpperCase()}${detail[0].slice(1)}`;
+    eval(`var ${detailType} = document.createElement('p')`);
     Object.assign( eval(detailType), {
-      className: detailType,
+      className: detail[0],
       textContent: detail[1]
     });
     eval(detailType).classList.add('expDetail');
-    eval(companyNameCollapsed + "SummaryRow").appendChild(eval(detailType));
+    eval(`${companyNameCollapsed}SummaryRow`).appendChild(eval(detailType));
   })
-  eval(companyNameCollapsed + "Container").appendChild(eval(companyNameCollapsed + "SummaryRow"));
+  eval(`${companyNameCollapsed}TextContainer`).appendChild(eval(`${companyNameCollapsed}SummaryRow`));
+
+  // Create position DOM
+  const positions = workParsed.filter(({name}) => name.match(RegExp(summary.name)));
+  var positionSummaryFields = {}
+  positions.forEach( (summary,index) => {
+    //Build positionSummaryFields 
+
+      
+      //Build expPositionContainer
+      eval(`var ${companyNameCollapsed}${index}PositionContainer = document.createElement('div')`);
+      Object.assign( eval(`${companyNameCollapsed}${index}PositionContainer`), {
+        className: 'expPositionContainer'
+      });
+      eval(eval(`${companyNameCollapsed}TextContainer`).appendChild(eval(`${companyNameCollapsed}${index}PositionContainer`)));
+      
+      //Build expPositionSummaryRow
+      
+      eval(`var ${companyNameCollapsed}${index}PositionSummaryRow = document.createElement('div')`);
+      Object.assign( eval(`${companyNameCollapsed}${index}PositionSummaryRow`), {
+        className: 'expPositionSummaryRow'
+      });
+      eval(`${companyNameCollapsed}${index}PositionContainer`).appendChild(eval(`${companyNameCollapsed}${index}PositionSummaryRow`));
+      
+      //Build details to go inside row
+      positionSummaryFields = {
+        "position": summary.position,
+        "startDate": summary.startDate,
+        "endDate": summary.endDate,
+        "durationPhrase": summary.durationPhrase
+      }
+      // Object.entries(positionSummaryFields).forEach( (position,index) => { //Iterate each detail
+        Object.entries(positionSummaryFields).forEach((positionDetail) => { //Iterate each detail
+          const detailType = `positonDetail${positionDetail[0].charAt(0).toUpperCase()}${positionDetail[0].slice(1)}`;
+          console.log({detailType})
+          eval(`var ${detailType} = document.createElement('p')`);
+          Object.assign( eval(detailType), {
+            className: detailType,
+            textContent: positionDetail[1]
+          });
+          eval(`${detailType}`).classList.add('positionDetail');
+          // eval(`${companyNameCollapsed}${index}PositionSummaryContainer`).appendChild(eval(`${companyNameCollapsed}${index}${detail}`));
+          eval(`${companyNameCollapsed}${index}PositionSummaryRow`).appendChild(eval(`${detailType}`));
+        })
+    // });
+  })
 });
+
+
+
+
 
 
 var main = document.querySelector('main');
 main.appendChild(expContainer);
 
+function calculateDuration(array) {
+  start = new Date(array.startDate);
+  end = new Date(array.endDate);
+  var durationYears;
+  var durationMonths;
+  var yearS = 's';
+  var monthS = 's';
+  if ( end.getMonth() < start.getMonth() ) {
+    durationMonths = 12 + end.getMonth() - start.getMonth();
+    durationYears = -1 + end.getFullYear() - start.getFullYear();
+  } else {
+    durationMonths = end.getMonth() - start.getMonth();
+    durationYears = end.getFullYear() - start.getFullYear();
+  }
+  if (durationYears == 1) {yearS = ''}
+  if (durationMonths == 1) {yearS = ''}
+  array.durationPhrase = `${durationYears} year${yearS}, ${durationMonths} month${monthS}`
+}
